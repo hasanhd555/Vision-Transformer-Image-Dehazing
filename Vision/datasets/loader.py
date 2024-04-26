dc3507c55c12db1df382bc8cff60236734167605
hasanhd555 <hasanhd555@gmail.com>
2024-04-26 19:08:12 +0500

    Applied clahe

--- Vision/datasets/loader.py
+++ Vision/datasets/loader.py
@@ -288,7 +288,7 @@ class PairLoader(Dataset):
 		target_img = read_img(os.path.join(self.root_dir, 'GT', img_name)) * 2 - 1
 
 		# Apply AHE to source and target images
-		source_img =source_img
+		source_img =apply_clahe(source_img)
 		
 		if self.mode == 'train':
 			[source_img, target_img] = augment([source_img, target_img], self.size, self.edge_decay, self.only_h_flip)
@@ -316,7 +316,7 @@ class SingleLoader(Dataset):
 		img = read_img(os.path.join(self.root_dir, img_name)) * 2 - 1
 
 		# Apply AHE to the image
-		img = img
+		img = apply_clahe(img)
 
 		return {'img': hwc_to_chw(img), 'filename': img_name}
 	
